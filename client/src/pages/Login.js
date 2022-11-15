5c8bbbe746b5ebc4e9489e587df192e7ba946283
MajkelBuszy <swierzewski.biz@gmail.com>
2022-11-15 19:00:34 +0100

    Added client side form validation

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,19 +1,26 @@
-import { Box, Button, Card, Container, TextField, Typography } from '@mui/material';
 import React, { useState, useContext } from 'react';
+import * as yup from 'yup';
 import { Link as RouterLink, useNavigate } from 'react-router-dom';
+import { Box, Button, Card, Container, TextField, Typography } from '@mui/material';
 import Link from '@mui/material/Link';
+import { Formik } from 'formik';
 
 import { AlertContext } from '../global/alert-context';
+import { loginSchema } from './FormModels/form-models';
+import { margin } from '@mui/system';
+
+const formValues = {
+    email: '',
+    password: ''
+}
 
 const Login = () => {
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
     const alert = useContext(AlertContext);
-
     const navigate = useNavigate();
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
+    const handleSubmit = (values) => {
         const loginUser = async () => {
             try {
                 const url1 = 'https://senior-dev-website-no-joke.herokuapp.com/api/users/login';
@@ -24,8 +31,8 @@ const Login = () => {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-                        email: email,
-                        password: password
+                        email: values.email,
+                        password: values.password
                     })
                 });
                 const responseData = await response.json();
@@ -56,53 +63,65 @@ const Login = () => {
 
     return (
         <Container component='main' maxWidth='xs' sx={{ mt: '100px' }}>
-            <Card sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column', p: '50px 30px' }} raised>
-                <Typography component='h1' variant='h5' color={'text.primary'}>
+            <Card sx={{ 
+                    display: 'flex', 
+                    justifyContent: 'center', 
+                    alignItems: 'center', 
+                    flexDirection: 'column', 
+                    p: '50px 30px'
+                }}
+                raised
+            >
+                <Typography gutterBottom component='h1' variant='h5' color={'text.primary'}>
                     Login
                 </Typography>
-                <Box component='form' onSubmit={handleSubmit} sx={{ mt: '10px' }}>
-                    <TextField 
-                        margin='normal'
-                        required
-                        fullWidth
-                        id='email'
-                        label="Email"
-                        name='email'
-                        autoComplete='email'
-                        type='text'
-                        value={email}
-                        onChange={(e) => {
-                            setEmail(e.target.value);
-                        }}
-                    />
-                    <TextField
-                        margin='normal'
-                        required
-                        fullWidth
-                        id='password'
-                        label="Password"
-                        name='password'
-                        autoComplete='current-password'
-                        type='password'
-                        value={password}
-                        onChange={(e) => {
-                            setPassword(e.target.value);
-                        }}
-                    />
-                    <Button
-                        type='submit'
-                        variant='contained'
-                        fullWidth
-                        sx={{ marginTop: '10px', marginBottom: '10px' }}
-                    >
-                        Login
-                    </Button>
-                    <Link component={RouterLink} to='/signup'>
-                        <Typography>
-                            Don't have an account? Sign Up
-                        </Typography>
-                    </Link>
-                </Box>
+                <Formik
+                    onSubmit={handleSubmit}
+                    initialValues={formValues}
+                    validationSchema={loginSchema}
+                >
+                    {({ values, errors, touched, handleBlur, handleChange, handleSubmit }) => (
+                        <Box component='form' onSubmit={handleSubmit} sx={{ '& > div': {margin: '15px 0'} }}>
+                            <TextField 
+                                fullWidth
+                                variant='outlined'
+                                type='email'
+                                label='Email'
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.email}
+                                name='email'
+                                error={!!touched.email && !!errors.email}
+                                helperText={touched.email && errors.email}
+                            />
+                            <TextField 
+                                fullWidth
+                                variant='outlined'
+                                type='password'
+                                label='Password'
+                                onBlur={handleBlur}
+                                onChange={handleChange}
+                                value={values.password}
+                                name='password'
+                                error={!!touched.password && !!errors.password}
+                                helperText={touched.password && errors.password}
+                            />
+                            <Button
+                                type='submit'
+                                variant='contained'
+                                fullWidth
+                                sx={{ marginTop: '10px', marginBottom: '10px' }}
+                            >
+                                Login
+                            </Button>
+                        </Box>
+                    )}
+                </Formik>
+                <Link component={RouterLink} to='/signup'>
+                    <Typography>
+                        Don't have an account? Sign Up
+                    </Typography>
+                </Link>
             </Card>
         </Container>
     )
