5669c7cc8cabcaa82495d279c74a3bfa39e8f836
MajkelBuszy <swierzewski.biz@gmail.com>
2022-11-21 22:20:30 +0100

    Update

--- client/src/components/ItemList.js
+++ client/src/components/ItemList.js
@@ -8,8 +8,6 @@ const ItemList = () => {
     useEffect(() => {
         const fetchRecentItems = async () => {
             try {
-                const url1 = 'https://senior-dev-website-no-joke.herokuapp.com/api/items/recent';
-                const url2 = 'http://localhost:5000/api/items/recent';
                 const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/items/recent`, {
                     method: 'GET'
                 });
